b9d748755af8c7003a72ccf25937e3de03ac4de3
BangAldy21 <anggachannel011@gmail.com>
2025-08-26 18:38:03 +0800

    Create tools.js

---
+++ src/data/tools.js
@@ -0,0 +1,26 @@
+export const tools = [
+{
+name: "React",
+icon: "/tools/react.png"
+},
+{
+name: "Vite",
+icon: "/tools/vite.png"
+},
+{
+name: "TailwindCSS",
+icon: "/tools/tailwind.png"
+},
+{
+name: "Firebase",
+icon: "/tools/firebase.png"
+},
+{
+name: "Three.js",
+icon: "/tools/three.png"
+},
+{
+name: "Framer Motion",
+icon: "/tools/framer.png"
+}
+];
